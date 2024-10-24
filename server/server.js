beee4f3938ff79b974ac5fc565e86209d1e77b49
Dip mondal <work.dipmondal@gmail.com>
2024-10-24 23:42:34 +0600

    server.js

--- server/server.js
+++ server/server.js
@@ -3,35 +3,47 @@ import express from "express";
 import cors from 'cors';
 import cookieParser from "cookie-parser";
 
-
-//local import
+// Local imports
 import authRouter from '../router/auth.router.js'
 import projectRouter from '../router/projects.router.js' 
 import blogRouter from '../router/blogPost.router.js'
 import ConnectDataBase from '../db/database.js'
-//env config
+
+// Environment configuration
 import dotenv from "dotenv";
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 
-
-//init express appp
+// Initialize express app
 const app = express();
 
-//use other dependence in express 
+// CORS options
+const corsOptions = {
+  origin: [
+    'http://localhost:5173', // Localhost
+    'https://dip-mondal.vercel.app' // Your Vercel domain
+  ],
+  credentials: true, // Allow credentials such as cookies, headers
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+};
+
+// Use middleware
+app.use(cors(corsOptions)); // Enable CORS with specific domains
 app.use(express.json());
 app.use(cookieParser());
 app.set('trust proxy', true);
 
+// Define routes
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/auth', blogRouter)
-app.use('/api/v1/auth', projectRouter)
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/blog', blogRouter);
+app.use('/api/v1/projects', projectRouter);
 
+// Start the server
 app.listen(PORT, () => {
-  ConnectDataBase()
-  console.log('Server is running on port',PORT);
+  ConnectDataBase();
+  console.log('Server is running on port', PORT);
 });
